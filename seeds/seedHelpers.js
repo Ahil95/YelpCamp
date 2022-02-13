51be73f9ac6050d5395ee2282a0a889faba22437
Ahil95 <88460486+Ahil95@users.noreply.github.com>
2022-02-13 19:16:32 +0530

    Initial commit

---
+++ seeds/seedHelpers.js
@@ -0,0 +1,22 @@
+module.exports.descriptors = [
+    'forest',
+    'Ancient',
+    'Petrified',
+    'Roaring',
+    'Cascade',
+    'Tumbling',
+    'Silent'
+
+]
+
+module.exports.places = [
+
+    'Flats',
+    'Villages',
+    'Group Camp',
+    'Horse Camp',
+    'Ghost Town',
+    'Camp',
+    'River'
+
+]
